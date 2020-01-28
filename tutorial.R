9d22b5506ada7927ccb2df3225de0c88aefb569a
Namchok <pech4751@gmail.com>
2020-01-28 22:33:36 +0700

    update Bar chart

--- tutorial.R
+++ tutorial.R
@@ -1,1 +1,54 @@
 # Tutorial for R language
+
+#---------------------
+# Library Section    |
+#---------------------
+
+# For graph
+library(ggplot2)
+
+#####################
+# Univariate Graphs #
+#####################
+
+#----------------
+# Bar chart     |
+#----------------
+
+# Load data from package "mosaicData"
+data(Marriage, package = "mosaicData") 
+
+# plot the distribution of race 
+ggplot(Marriage, aes(x = race)) + geom_bar() 
+
+# plot the distribution of race with modified colors and labels
+ggplot(Marriage, aes(x = race)) + 
+  geom_bar(fill = "cornflowerblue", color="black") + 
+  labs(x = "Race", y = "Frequency", title = "Participants by race") 
+
+# More color
+ggplot(Marriage, aes(x = race)) + 
+  geom_bar(fill = c("red", "blue", "green", "black"), color="black") + 
+  labs(x = "Race", y = "Frequency", title = "Participants by race") 
+
+# Label center
+ggplot(Marriage, aes(x = race)) + 
+  geom_bar(fill = c("red", "blue", "green", "black"), color="black") + 
+  labs(x = "Race", y = "Frequency", title = "Participants by race") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
