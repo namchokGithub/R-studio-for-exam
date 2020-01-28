7c2e4367e7e636f6d1eb89634cb9ef80ef7eccf3
Namchok <pech4751@gmail.com>
2020-01-28 23:56:33 +0700

    update add Qualitative

--- tutorial.R
+++ tutorial.R
@@ -8,10 +8,19 @@
 library(ggplot2)
 # 
 library(dplyr) 
+# Tree map
+library(treemapify) 
+# Scales
+library(scales) 
+
 
 #####################
 # Univariate Graphs #
 #####################
+
+###############
+# Categorical #
+###############
 
 #----------------
 # Bar chart     |
@@ -113,6 +122,128 @@ ggplot(Marriage, aes(x = officialTitle)) +
   geom_bar() + 
   labs(x = "", y = "Frequency", title = "Marriages by officiate") + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#----------------
+# Pie chart     |
+#----------------
+
+# create a basic ggplot2 pie chart 
+plotdata <- Marriage %>% 
+            count(race) %>% 
+            arrange(desc(race)) %>%
+            mutate(prop = round(n * 100 / sum(n), 1), lab.ypos = cumsum(prop) - 0.5 * prop) 
+
+ggplot(plotdata, aes(x = "", y = prop, fill = race)) + 
+  geom_bar(width = 1, stat = "identity", color = "black") + 
+  coord_polar("y", start = 0, direction = -1) + 
+  theme_void()
+
+# create a pie chart with slice labels 
+plotdata <- Marriage %>% 
+            count(race) %>% 
+            arrange(desc(race)) %>% 
+            mutate(prop = round(n*100/sum(n), 1), lab.ypos = cumsum(prop) - 0.5*prop) 
+
+plotdata$label <- paste0(plotdata$race, "\n",round(plotdata$prop), "%") 
+
+ggplot(plotdata, aes(x = "", y = prop, fill = race)) + 
+  geom_bar(width = 1, stat = "identity", color = "black") + 
+  geom_text(aes(y = lab.ypos, label = label), color = "black") + 
+  coord_polar("y", start = 0,direction = -1) + 
+  theme_void() + 
+  theme(legend.position = "FALSE") + labs(title = "Participants by race")
+
+#----------------
+# Tree map      |
+#----------------
+
+# create a treemap of marriage officials 
+plotdata <- Marriage %>% count(officialTitle) 
+
+ggplot(plotdata, aes(fill = officialTitle, area = n)) + 
+  geom_treemap() +
+  labs(title = "Marriages by officiate")
+
+# create a treemap with tile labels 
+ggplot(plotdata, aes(fill = officialTitle, area = n, label = officialTitle)) +
+  geom_treemap() + geom_treemap_text(colour = "white", place = "centre") + 
+  labs(title = "Marriages by officiate") + theme(legend.position = "none") 
+
+
+###############
+# Qualitative #
+###############
+
+#----------------
+# Histogram     |
+#----------------
+
+# plot the age distribution using a histogram 
+ggplot(Marriage, aes(x = age)) + 
+  geom_histogram() + 
+  labs(title = "Participants by age", x = "Age") 
+
+# plot the histogram with blue bars and white borders 
+ggplot(Marriage, aes(x = age)) + 
+  geom_histogram(fill = "cornflowerblue", color = "white") + 
+  labs(title="Participants by age", x = "Age") 
+
+#---------------------------
+# Bins and bandwidths      |
+#---------------------------
+
+# plot the histogram with 20 bins 
+ggplot(Marriage, aes(x = age)) + 
+  geom_histogram(fill = "cornflowerblue", color = "white", bins = 20) + 
+  labs(title="Participants by age", subtitle = "number of bins = 20", x = "Age") 
+
+# plot the histogram with a binwidth of 5 
+ggplot(Marriage, aes(x = age)) + 
+  geom_histogram(fill = "cornflowerblue", color = "white", binwidth = 5) + 
+  labs(title="Participants by age", subtitle = "binwidth = 5 years", x = "Age") 
+
+# plot the histogram with percentages on the y-axis 
+ggplot(Marriage, aes(x = age, y= ..count.. / sum(..count..))) + 
+  geom_histogram(fill = "cornflowerblue", color = "white", binwidth = 5) + 
+  labs(title="Participants by age", y = "Percent", x = "Age") + 
+  scale_y_continuous(labels = percent)
+
+#-------------------
+# Density plot     |
+#-------------------
+
+# Create a kernel density plot of age 
+ggplot(Marriage, aes(x = age)) + 
+  geom_density() +    
+  labs(title = "Participants by age")
+
+# Create a kernel density plot of age 
+ggplot(Marriage, aes(x = age)) + 
+  geom_density(fill = "indianred3") + 
+  labs(title = "Participants by age") 
+
+# default bandwidth for the age variable 
+bw.nrd0(Marriage$age) 
+## [1] 5.181946
+
+# Create a kernel density plot of age 
+ggplot(Marriage, aes(x = age)) + 
+  geom_density(fill = "deepskyblue", bw = 1) + 
+  labs(title = "Participants by age", subtitle = "bandwidth = 1")
+
+#-------------
+# Dot Chart  |
+#-------------
+
+# plot the age distribution using a dotplot 
+ggplot(Marriage, aes(x = age)) + 
+  geom_dotplot() + 
+  labs(title = "Participants by age", y = "Proportion", x = "Age")
+
+# Plot ages as a dot plot using gold dots with black borders 
+ggplot(Marriage, aes(x = age)) + 
+  geom_dotplot(fill = "gold", color = "black") + 
+  labs(title = "Participants by age", y = "Proportion", x = "Age") 
 
 
 
