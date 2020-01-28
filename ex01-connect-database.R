f94d5c8b3b8aec8ae9ddda3deb0de6ea79360e5f
Namchok <pech4751@gmail.com>
2020-01-29 02:02:03 +0700

    update connect database

--- ex01-connect-database.R
+++ ex01-connect-database.R
@@ -1,1 +1,20 @@
 # Connect Database
+library(RMySQL)
+
+# utf8
+Sys.setlocale("LC_CTYPE", "UTF-8")
+# options(encoding="UTF-8")
+
+#------------ Connect database -------------------#
+
+con = dbConnect(MySQL(),
+                user="oesuser",
+                password="tsp60@oesuser",
+                dbname="tsp60_nu_oesdb",
+                host="10.80.39.17")
+
+dataFormDB <- dbGetQuery(con, "SELECT * FROM oes_result_exam")
+dataFormDB
+dbDisconnect(con)
+
+#------------ Connect database -------------------#
